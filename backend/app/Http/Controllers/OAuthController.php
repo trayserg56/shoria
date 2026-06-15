da7b32e3985bdc92108e29ee2c799dbeec281baf
Trayserg <trayserg@gmail.com>
2026-06-16 02:20:17 +0500

    Починить VK OAuth: убрать scope=email, не поддерживается VK ID

--- backend/app/Http/Controllers/OAuthController.php
+++ backend/app/Http/Controllers/OAuthController.php
@@ -21,7 +21,7 @@ class OAuthController extends Controller
             throw new HttpException(503, 'VK OAuth is not configured.');
         }
 
-        return Socialite::driver(self::PROVIDER_VK)->redirect();
+        return Socialite::driver(self::PROVIDER_VK)->scopes([])->redirect();
     }
 
     public function handleVkCallback(): RedirectResponse
